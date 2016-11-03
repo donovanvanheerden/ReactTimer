f5de28150344f640215989530f5f5b05852d7ace
Donovan van Heerden <dp.vanheerden@gmail.com>
2016-11-03 06:30:43 +0200

    added countdown functionality and countdown tests

--- app/components/Countdown.jsx
+++ app/components/Countdown.jsx
@@ -4,13 +4,42 @@ var CountdownForm = require('CountdownForm');
 
 var Countdown = React.createClass({
   getInitialState: function() {
-    return {count: 0}
+    return {
+      count: 0,
+      countdownStatus: 'stopped'
+    }
+  },
+  componentDidUpdate: function(prevProps, prevState) {
+    if (this.state.countdownStatus !== prevState.countdownStatus) {
+      switch(this.state.countdownStatus) {
+        case 'stopped':
+          break;
+        case 'started':
+          this.startTimer();
+          break;
+        case 'paused':
+          break;
+      }
+    }
+  },
+  startTimer: function() {
+    this.timer = setInterval(() => {
+      var newCount = this.state.count - 1;
+      if (newCount == 0) {
+        clearInterval(this.timer);
+        this.setState({count: newCount, countdownStatus: 'stopped'});
+      } else {
+        this.setState({count: newCount});
+      }
+
+    }, 1000);
   },
   handleSetCountdown: function(seconds) {
-    this.setState({count: seconds});
+    this.setState({count: seconds, countdownStatus: 'started'});
   },
   render: function() {
     var {count} = this.state;
+
     return (
       <div>
         <Clock totalSeconds={count}/>
